33139c0a51d34d7e510b6112ec9d68c482127de9
Heike Görzig <heike.goerzig@helmholtz-berlin.de>
2022-01-12 17:06:25 +0100

    Added entry point for console script

--- code/nxsOnto/setup.py
+++ code/nxsOnto/setup.py
@@ -6,11 +6,21 @@
 #
 from setuptools import setup
 
+from setuptools import setup
+
+__entry_points__ = {
+    "console_scripts": [
+        "nexusontology = nxsOnto.generator:main",
+    ],
+    # 'gui_scripts': [],
+}
+
 setup(name='NeXusOntologyGenerator',
       version='1.1',
       description='Generates an ontology from nxdl files',
       author='Steve Collins',
       url="https://github.com/nexusformat/NeXusOntology/code/nxsOnto",
       packages=['nxsOnto'],
-      license='Apache 2'
+      license='Apache 2',
+      entry_points=__entry_points__,
       )
